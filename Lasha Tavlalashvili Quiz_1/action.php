ea6b81e47dc50c66175ed46ab0303221b0acaa20
TavlaLasha <lashatavlalashvili@yahoo.com>
2020-04-09 19:59:24 +0400

    Quiz_1

---
+++ Lasha Tavlalashvili Quiz_1/action.php
@@ -0,0 +1,37 @@
+<table cellspacing=0>
+    <tr>
+        <td colspan="2" style="font-weight:bold; background:silver">მონაცემები</td>
+    </tr>
+    <tr>
+        <td>სახელი</td>
+        <td><?=$name?></td>
+    </tr>
+    <tr>
+        <td>გვარი</td>
+        <td><?=$lastname?></td>
+    </tr>
+    <tr>
+        <td>დაბადების თარიღი</td>
+        <td><?=$birthdate?></td>
+    </tr>
+    <tr>
+        <td>პირადი ნომერი</td>
+        <td><?=$id?></td>
+    </tr>
+    <tr>
+        <td>მისამართი</td>
+        <td><?=$address?></td>
+    </tr>
+    <tr>
+        <td>რეგისტრაციის თარიღი</td>
+        <td><?=$regdate?></td>
+    </tr>
+    <tr>
+        <td>მობილური</td>
+        <td><?=$mobile?></td>
+    </tr>
+    <tr>
+        <td>დამატებითი ინფორმაცია</td>
+        <td><?=$adinfo?></td>
+    </tr>
+</table>
---
+++ Lasha Tavlalashvili Quiz_1/info.php
@@ -0,0 +1,74 @@
+<?php
+    
+
+    $infname = "";
+    $inflastname = "";
+    $infbirthdate = "";
+    $infid = "";
+    $infaddress = "";
+
+    if(isset($_POST["sub"])){
+        $name = $_POST["name"];
+        $lastname = $_POST["lastname"];
+        $birthdate = $_POST["birthdate"];
+        $id = $_POST["id"];
+        $address = $_POST["address"];
+        $regdate = $_POST["regdate"];
+        $mobile = $_POST["mobile"];
+        $adinfo = $_POST["adinfo"];
+        $preDate = mktime(0, 0, 0, 1, 1, 1950);
+        $checkBool = true;
+        $newDate = strtotime($birthdate);
+        
+        if(!isset($name) || trim($name) == "" || strlen($name)<2 || strlen($name)>20){
+            $infname="შეცდომა";
+            $checkBool = false;
+        }
+        if(!isset($lastname) || trim($lastname) == "" || strlen($lastname)<3 || strlen($lastname)>50){
+            $inflastname="შეცდომა";
+            $checkBool = false;
+        }
+        if(!isset($birthdate) || trim($birthdate) == "" || $newDate < $preDate){
+            $infbirthdate="შეცდომა";
+            $checkBool = false;
+        }
+        if(!isset($id) || trim($id) == "" || strlen($id)!=11 || !is_numeric($id)){
+            $infid="შეცდომა";
+            $checkBool = false;
+        }
+        if(!isset($address) || trim($address) == "" || strlen($address)>70){
+            $infaddress="შეცდომა";
+            $checkBool = false;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Quiz_1</title>
+</head>
+<body>
+    <h1>Form</h1>
+    <form action="" method="post">
+        სახელი - <input type="text" name="name"><span><?="* ".$infname?></span><br><br>
+        გვარი - <input type="text" name="lastname"><span><?="* ".$inflastname?></span><br><br>
+        დაბადების თარიღი - <input type="date" name="birthdate"><span><?="* ".$infbirthdate?></span><br><br>
+        პირადი ნომერი - <input type="text" name="id"><span><?="* ".$infid?></span><br><br>
+        მისამართი - <input type="text" name="address"><span><?="* ".$infaddress?></span><br><br>
+        რეგისტრაციის თარიღი - <input type="date" name="regdate"><br><br>
+        მობილური - <input type="tel" name="mobile"><br><br>
+        დამატებითი ინფორმაცია - <input type="text" name="adinfo"><br><br>
+        <input type="submit" value="შეყვანა" name="sub">
+    </form>
+    <?php
+        if(isset($_POST["sub"])){
+            if($checkBool){
+                include "action.php";
+            }
+        }
+    ?>
+</body>
+</html>
